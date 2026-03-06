3bd90991f5986afa04bfef1a9862e4554c84f3b8
BradOlv <boliva-2023502@kinal.edu.gt>
2026-03-06 16:26:55 -0600

    T18: crear validaciones para el módulo de la entidad ServiceRequest (serviceRequest-validator) para Admin

--- src/ServiceRequest/serviceRequest.routes.js
+++ src/ServiceRequest/serviceRequest.routes.js
@@ -1,11 +1,12 @@
-
 import { Router } from 'express';
 import { getAllRequestsAdmin, deleteRequestAdmin } from './serviceRequest.controller.js';
 import { validateJWT, isAdmin } from '../../middlewares/validate-jwt.js';
+import { validateServiceRequestId } from '../../middlewares/serviceRequest-validator.js';
 
 const router = Router();
 
+//  Rutas protegidas para ADMIN
 router.get('/', [validateJWT, isAdmin], getAllRequestsAdmin);
-router.delete('/:id', [validateJWT, isAdmin], deleteRequestAdmin);
+router.delete('/:id', [validateJWT, isAdmin, validateServiceRequestId], deleteRequestAdmin);
 
 export default router;
