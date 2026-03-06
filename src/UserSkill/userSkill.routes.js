692e5c283b3a1bbd7872e84671b8935ba0f62866
BradOlv <boliva-2023502@kinal.edu.gt>
2026-03-06 16:27:48 -0600

    T24: crear validaciones para el módulo de la entidad UserSkill (userSkill-validator) para Admin

--- src/UserSkill/userSkill.routes.js
+++ src/UserSkill/userSkill.routes.js
@@ -5,6 +5,8 @@ import { validateJWT, isAdmin } from '../../middlewares/validate-jwt.js';
 
 const router = Router();
 
+// Ver todas las habilidades vinculadas a usuarios
+
 router.get('/', [validateJWT, isAdmin], getAllUserSkillsAdmin);
 
 export default router;
