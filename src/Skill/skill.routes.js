6ba9d068e010458dd48a455413dd450d7047e75d
BradOlv <boliva-2023502@kinal.edu.gt>
2026-03-06 16:27:42 -0600

    T21: crear validaciones para el módulo de la entidad Skill (skill-validator) para Admin

--- src/Skill/skill.routes.js
+++ src/Skill/skill.routes.js
@@ -2,13 +2,14 @@
 import { Router } from 'express';
 import { createSkill, updateSkill, deactivateSkill, getAllSkillsAdmin } from './skill.controller.js';
 import { validateJWT, isAdmin } from '../../middlewares/validate-jwt.js';
+import { validateCreateSkill, validateSkillId } from '../../middlewares/skill-validator.js';
 
 const router = Router();
 
 // Todas las rutas de Admin requieren Token y Rol de Admin
-router.post('/', [validateJWT, isAdmin], createSkill);
-router.put('/:id', [validateJWT, isAdmin], updateSkill);
-router.patch('/:id', [validateJWT, isAdmin], deactivateSkill); // Patch porque es desactivación parcial
+router.post('/', [validateJWT, isAdmin, validateCreateSkill], createSkill);
+router.put('/:id', [validateJWT, isAdmin, validateSkillId, validateCreateSkill], updateSkill);
+router.patch('/:id', [validateJWT, isAdmin, validateSkillId], deactivateSkill);
 router.get('/', [validateJWT, isAdmin], getAllSkillsAdmin);
 
 export default router;
