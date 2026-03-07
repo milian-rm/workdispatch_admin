61dce67de0cc28ca7c6c452ec6688f745399d57c
DevJosL <jmoran-2022388@kinal.edu.gt>
2026-03-06 20:20:54 -0600

    T9/ crear validaciones para el módulo de la entidad User (user-validator)

--- src/Users/user.controller.js
+++ src/Users/user.controller.js
@@ -1,10 +1,9 @@
-'use strict';
-
 import User from './user.model.js';
+import { cloudinary } from '../../middlewares/file-uploader.js';
+
 
 export const getUsers = async (req, res) => {
     try {
-
         const users = await User.find();
 
         res.status(200).json({
@@ -21,18 +20,128 @@ export const getUsers = async (req, res) => {
     }
 };
 
+export const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'Usuario no encontrado'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error al obtener usuario',
+            error: error.message
+        });
+    }
+};
+
+export const createUser = async (req, res) => {
+    try {
+        const data = req.body;
+
+        if (req.file){
+            const extension = req.file.path.split('.').pop();
+            const filename = req.file.filename;
+            const relativePath = filename.substring(filename.indexOf('users/'));
+
+            data.profilePhoto = `${relativePath}.${extension}`;
+        } else {
+            data.profilePhoto = `users/default-profile.png`;
+        }
+
+        const existingUser = await User.findOne({ email: data.email });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: 'El correo ya está registrado'
+            });
+        }
+
+        const user = new User(data);
+        await user.save();
+
+        res.status(201).json({
+            success: true,
+            message: 'Usuario creado correctamente',
+            data: user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error al crear usuario',
+            error: error.message
+        });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+
+        if (req.file){
+            const currentUser = await User.findById(id);
+
+            if (currentUser && currentUser.profilePhoto) {
+                const photoPath = currentUser.profilePhoto;
+                const photoWithoutExt = photoPath.substring(0, photoPath.lastIndexOf('.'));
+            };
+            const publicId = `workDispatch/${photoWithoutExt}`;
+
+            try {
+                await cloudinary.uploader.destroy(publicId);
+            } catch (deleteError) {
+                console.error('Error al eliminar imagen anterior:', deleteError);
+            }
+        }
+
+        const userExist = await User.findById(id);
+        if (!userExist) {
+            return res.status(404).json({
+                success: false,
+                message: 'Usuario no encontrado'
+            });
+        }
+
+        const userUpdated = await User.findByIdAndUpdate(
+            id,
+            data,
+            { new: true, runValidators: true }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: 'Usuario actualizado correctamente',
+            data: userUpdated
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error al actualizar usuario',
+            error: error.message
+        });
+    }
+};
+
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
         const user = await User.findOne({ email, password });
-
-        if (user.active === false) {
-            return res.status(403).json({
-                success: false,
-                message: 'Usuario inactivo'
-            });
-        }
 
         if (!user) {
             return res.status(401).json({
@@ -58,6 +167,7 @@ export const login = async (req, res) => {
         res.status(200).json({
             success: true,
             message: 'Inicio de sesión de administrador exitoso',
+            data: user
         });
 
     } catch (error) {
@@ -71,7 +181,6 @@ export const login = async (req, res) => {
 
 export const changeUserStatus = async (req, res) => {
     try {
-
         const { id } = req.params;
 
         const user = await User.findById(id);
@@ -84,7 +193,6 @@ export const changeUserStatus = async (req, res) => {
         }
 
         user.active = !user.active;
-
         await user.save();
 
         res.status(200).json({
--- src/Users/user.routes.js
+++ src/Users/user.routes.js
@@ -1,12 +1,32 @@
-'use strict';
-
 import { Router } from 'express';
-import { getUsers, login, changeUserStatus } from './user.controller.js';
+import {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    login,
+    changeUserStatus
+} from './user.controller.js';
+import {
+    validateAdminLogin,
+    validateCreateUser,
+    validateUpdateUser,
+    validateUserIdParam,
+    validateChangeUserStatus
+} from '../../middlewares/user-validator.js';
+
+import { uploadUserProfileImage } from '../../middlewares/file-uploader.js';
+
 
 const router = Router();
 
 router.get('/', getUsers);
-router.post('/login', login);
-router.patch('/:id/status', changeUserStatus);
+router.get('/:id', validateUserIdParam, getUserById);
+
+router.post('/', validateCreateUser, uploadUserProfileImage.single('profilePhoto'), createUser);
+router.post('/login', validateAdminLogin, login);
+
+router.put('/:id', validateUpdateUser, uploadUserProfileImage.single('profilePhoto'), updateUser);
+router.patch('/:id/status', validateChangeUserStatus, changeUserStatus);
 
 export default router;
