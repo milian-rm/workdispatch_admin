5f098515a8bf5ea72f9a219e3f948c81da65a5b5
DevJosL <jmoran-2022388@kinal.edu.gt>
2026-03-06 23:09:23 -0600

    T15/ Crear validaciones para el módulo de la entidad Category (category-validator)

--- src/Categories/category.routes.js
+++ src/Categories/category.routes.js
@@ -6,13 +6,19 @@ import {
     updateCategory,
     changeCategoryStatus
 } from './category.controller.js';
+import {
+    validateCreateCategory,
+    validateUpdateCategory,
+    validateCategoryIdParam,
+    validateChangeCategoryStatus
+} from '../../middlewares/category-validator.js';
 
 const router = Router();
 
 router.get('/', getCategories);
-router.get('/:id', getCategoryById);
-router.post('/', createCategory);
-router.put('/:id' , updateCategory);
-router.patch('/:id/status', changeCategoryStatus);
+router.get('/:id', validateCategoryIdParam, getCategoryById);
+router.post('/', validateCreateCategory, createCategory);
+router.put('/:id', validateUpdateCategory, updateCategory);
+router.patch('/:id/status', validateChangeCategoryStatus, changeCategoryStatus);
 
 export default router;
